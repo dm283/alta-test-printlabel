b988be959ea47df1e24b30ffaa1f3c7b38596504
dm283 <dm283@yandex.ru>
2024-05-13 09:20:30 +0530

    the latest ver

--- test-app-v2.py
+++ test-app-v2.py
@@ -1,35 +1,51 @@
-import sys, asyncio, random, time, websockets, json, logging
+import sys, asyncio, random, time, websockets, json, logging, os, configparser
 from colorama import just_fix_windows_console, Fore, Back, Style
+from pathlib import Path
 
 just_fix_windows_console()
+
+config = configparser.ConfigParser()
+config_file = os.path.join(Path(__file__).resolve().parent, 'config.ini')
+if os.path.exists(config_file):
+  config.read(config_file, encoding='utf-8')
+else:
+  print("error! config file doesn't exist"); sys.exit()
+
+
+def get_code_list():
+    # gets list of code from outer source/file
+    code_list = ['4680648061203', '4680648061200', ]
+    return code_list
+
 
 # logger = logging.getLogger('websockets')
 # logger.setLevel(logging.DEBUG)
 # logger.addHandler(logging.StreamHandler())
 
-CHECK_AUTH = False
-PASS_IF_TASK_ERROR = False
-INSTANCES_COLORS_ENABLED = True
+CHECK_AUTH = config.getboolean('default', 'check_auth')
+PASS_IF_TASK_ERROR = config.getboolean('default', 'pass_if_task_error')
+INSTANCES_COLORS_ENABLED = config.getboolean('default', 'instances_colors_enabled')
+CNT_INSTANCES = int(config['default']['cnt_instances'])
+CNT_TASKS = int(config['default']['cnt_tasks'])
+CNT_CYCLES = int(config['default']['cnt_cycles'])
+REQUEST_CYCLE_FREQUENCY = int(config['default']['request_cycle_frequency'])  # seconds
+
+TOKEN = config['default']['token']
+URI = config['default']['uri']
+PROTOCOL_GUID = config['default']['protocol_guid']
+ORDER_GUID = config['default']['order_guid']
+STATISTIC = config.getboolean('default', 'statistic')
+
 COLOR_SET = {
     1: Fore.YELLOW,
     2: Fore.CYAN,
     3: Fore.LIGHTGREEN_EX,
 }
-CNT_INSTANCES = 3
-CNT_TASKS = 2
 CNT_INSTANCES_STARTED = int()
-CNT_CYCLES = 10  #
-REQUEST_CYCLE_FREQUENCY = 1  # seconds
-
-TOKEN = 'bqapx1cgfyaugy2qgi30f'
-URI = "wss://ts.a-m0.ru/ws/"
-PROTOCOL_GUID = '2bfc39a0-080a-11ef-a7a0-bb86ff8a3b53'
-ORDER_GUID = '2c68e830-0467-11ef-9d32-47ff33ac6ce5'
-STATICTIC = True
-
 CYCLE_START_TIME = {}
 CNT_ERRORS = int()
 DURATION_LIST = list()
+CODE_LIST = get_code_list()
 
 
 async def display_instance_text(instance_id, instance_color, text_to_display):
@@ -38,11 +54,10 @@ async def display_instance_text(instance_id, instance_color, text_to_display):
 
 
 async def receive_response(ws, instance_id, instance_color, cnt_tsks):
-    #
+    # receives all responses for instance
     global CNT_ERRORS
 
     for cycle in range(CNT_CYCLES):
-
         for i in range(cnt_tsks):
             #await asyncio.sleep(2) ######################
             response = await ws.recv()
@@ -57,19 +72,21 @@ async def receive_response(ws, instance_id, instance_color, cnt_tsks):
             DURATION_LIST.append(duration)
         
 
-
 async def create_request(ws, instance_id, instance_color, cnt_tsks):
-    #
-    code = "4680648061203"
+    # creates all requests for instance
     json_msg = { "Operation": "PrintLabel", "Data": {
-                "Code": code, "ProtocolGUID": PROTOCOL_GUID, "OrderGUID": ORDER_GUID, "Statistic": STATICTIC, } }
-    msg = json.dumps(json_msg)
+                "Code": '', "ProtocolGUID": PROTOCOL_GUID, "OrderGUID": ORDER_GUID, "Statistic": STATISTIC, } }
     CYCLE_START_TIME[instance_id] = {}
 
 
     for cycle in range(CNT_CYCLES):
 
         for i in range(cnt_tsks):
+            # each iteration chooses random code from code_list
+            code = random.choice(CODE_LIST)
+            json_msg['Data']['Code'] = code
+            msg = json.dumps(json_msg)
+
             await ws.send(msg)
             request_time = time.monotonic()
             text_msg = f'[ request ]:  {request_time}  {msg}'
@@ -91,7 +108,6 @@ async def instance_action_v1():
     instance_color = COLOR_SET[instance_id] if INSTANCES_COLORS_ENABLED else Fore.WHITE
     
     await display_instance_text(instance_id, instance_color, 'instance is created')
-
     await display_instance_text(instance_id, instance_color, 'set connection...')
     async with websockets.connect(uri=URI, subprotocols=['chat',]) as ws:
         await display_instance_text(instance_id, instance_color, 'ok. connected')
@@ -107,31 +123,18 @@ async def instance_action_v1():
             print('ERROR:  the user is not authorised!')
             sys.exit()
 
-
         # 2 Printlabel requests and responses
-        # for i in range(CNT_CYCLES):
-        # turn_time = time.monotonic()
-        # text_msg = f'turn {i+1} tasks created  {turn_time}'
-        # await display_instance_text(instance_id, instance_color, text_msg)
         await asyncio.gather(
             asyncio.create_task( create_request(ws, instance_id, instance_color, CNT_TASKS) ),
             asyncio.create_task( receive_response(ws, instance_id, instance_color, CNT_TASKS) ),
         )
-            #text_msg = f'turn {i+1} tasks created  {turn_time}'
-            #await display_instance_text(instance_id, instance_color, text_msg)
-        # await asyncio.sleep(REQUEST_CYCLE_FREQUENCY)
 
     await asyncio.sleep(0.5)
     await display_instance_text(instance_id, instance_color, 'all tasks is completed')
     
 
-async def main():
-    # the main entry function
-    # runs setted number of instances and its tasks
-    start_test_time = time.monotonic()
-    await asyncio.gather( *( instance_action_v1() for i in range(CNT_INSTANCES) ) )
-    finish_test_time = time.monotonic()
-    test_duration = round( (finish_test_time - start_test_time), 3)
+async def display_report(test_duration):
+    # creates and displays a report
     print(Style.RESET_ALL)
     report_color = Fore.LIGHTMAGENTA_EX
     print(report_color + '*****************         PROGRESS REPORT        *****************')
@@ -143,9 +146,21 @@ async def main():
     if DURATION_LIST:
         print(report_color + '[               Response time (MIN)             ] =', min(DURATION_LIST), 'seconds')
         print(report_color + '[               Response time (MAX)             ] =', max(DURATION_LIST), 'seconds')
-        print(report_color + '[               Response time (AVG)             ] =', round(sum(DURATION_LIST)/len(DURATION_LIST), 3), 'seconds')
-        print(Fore.LIGHTCYAN_EX + '[               Response time list              ] =', DURATION_LIST)
+        print(report_color + '[               Response time (AVG)             ] =', 
+              round(sum(DURATION_LIST)/len(DURATION_LIST), 3), 'seconds')
+    print(Fore.LIGHTCYAN_EX + '[               Response time list              ] =', DURATION_LIST)
     print(Style.RESET_ALL)
+
+
+async def main():
+    # the main entry function - runs setted number of instances and its tasks
+    start_test_time = time.monotonic()
+    await asyncio.gather( *( instance_action_v1() for i in range(CNT_INSTANCES) ) )
+    finish_test_time = time.monotonic()
+    test_duration = round( (finish_test_time - start_test_time), 3)
+    await display_report(test_duration)
+
+
 
 if __name__ == '__main__':
     asyncio.run(main())
